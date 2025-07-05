8ef2105a3a96aa2a3e9f7186a7cb03c3287f51f0
Albert Hopkins <marduk@letterboxes.org>
2025-07-05 06:11:22 -0500

    tests.test_cli: add fixture for cli.ACTIONS

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,24 +4,37 @@
 
 import argparse
 import unittest
+from typing import Any, Callable, TypeAlias
 from unittest import mock
+
+from unittest_fixtures import FixtureContext, Fixtures, fixture, given, where
 
 from gbp_webhook import cli
 
-patch = mock.patch
+Actions: TypeAlias = dict[str, Callable[[argparse.Namespace], Any]]
 
 
-@patch.dict(cli.ACTIONS, {"serve": mock.Mock()})
+@fixture()
+def cli_actions(
+    _: Fixtures,
+    cli_actions: Actions | None = None,  # pylint: disable=redefined-outer-name
+) -> FixtureContext[mock.Mock]:
+    with mock.patch.dict(cli.ACTIONS, cli_actions or {}) as mock_obj:
+        yield mock_obj
+
+
+@given(cli_actions)
+@where(cli_actions={"serve": mock.Mock()})
 class HanderTests(unittest.TestCase):
-    def test(self) -> None:
+    def test(self, fixtures: Fixtures) -> None:
         parser = argparse.ArgumentParser()
         cli.parse_args(parser)
         args = parser.parse_args(["-p", "6000", "serve", "--allow", "0.0.0.0"])
 
         cli.handler(args, mock.Mock(), mock.Mock())
 
-        # pylint: disable=no-member
-        cli.ACTIONS["serve"].assert_called_once_with(args)
+        actions = fixtures.cli_actions
+        actions["serve"].assert_called_once_with(args)
 
 
 class ParseArgsTests(unittest.TestCase):
