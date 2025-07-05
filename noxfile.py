b496e7921be9154f6dbc68a8f8e4e1a933a9f417
Albert Hopkins <marduk@letterboxes.org>
2025-07-05 07:38:14 -0500

    noxfile: install (all) dev dependencies
    Install all dev dependencies (e.g. unittest-fixtures).

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 """noxfile for ci/cd testing"""
+
 # pylint: disable=missing-docstring
 import nox
 
@@ -6,7 +7,11 @@ import nox
 @nox.session(python=("3.12", "3.13", "3.14"))
 @nox.parametrize("gbpcli", ["stable", "scm"])
 def tests(session: nox.Session, gbpcli: str) -> None:
-    session.install(".", "coverage")
+    session.install(".")
+    dev_dependencies = nox.project.load_toml("pyproject.toml")["dependency-groups"][
+        "dev"
+    ]
+    session.install(*dev_dependencies)
 
     if gbpcli == "scm":
         session.install("-U", "git+https://github.com/enku/gbpcli.git")
